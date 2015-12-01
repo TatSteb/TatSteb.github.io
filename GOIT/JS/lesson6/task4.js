983b2ecde97318c56c2224a0792d24656ddd9a4e
TatSteb <12mailme21@gmail.com>
2015-12-01 19:04:16 +0200

    lesson 6

--- GOIT/JS/lesson6/task4.js
+++ GOIT/JS/lesson6/task4.js
@@ -11,6 +11,7 @@
 
  Выведите список имён в массиве после сортировки.
  */
+
 var vasya = { name: 'Вася', age: 23 };
 var masha = { name: 'Маша', age: 18 };
 var vovochka = { name: 'Вовочка', age: 6 };
