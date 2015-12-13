e3887241da338f2dbb23c20cafd464521d4c6038
TatSteb <12mailme21@gmail.com>
2015-12-13 18:31:51 +0200

    lesson 14 is finished

---
+++ GOIT/JS/lesson14/task4.js
@@ -0,0 +1,50 @@
+/**
+ Есть класс CoffeeMachine, заданный в функциональном стиле.
+ Задача: переписать CoffeeMachine в виде класса с использованием прототипа.
+ Исходный код:
+ function CoffeeMachine(power) {
+  var waterAmount = 0;
+  var WATER_HEAT_CAPACITY = 4200;
+  function getTimeToBoil() {
+    return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+  }
+  this.run = function() {
+    setTimeout(function() {
+      alert( 'Кофе готов!' );
+    }, getTimeToBoil());
+  };
+  this.setWaterAmount = function(amount) {
+    waterAmount = amount;
+  };
+}
+ var coffeeMachine = new CoffeeMachine(10000);
+ coffeeMachine.setWaterAmount(50);
+ coffeeMachine.run();
+ */
+
+function CoffeeMachine(power) {
+    // свойства конкретной кофеварки
+    this._power = power;
+    this._waterAmount = 0;
+}
+
+// свойства и методы для всех объектов класса
+CoffeeMachine.prototype.WATER_HEAT_CAPACITY = 4200;
+
+CoffeeMachine.prototype._getTimeToBoil = function() {
+    return this._waterAmount * this.WATER_HEAT_CAPACITY * 80 / this._power;
+};
+
+CoffeeMachine.prototype.run = function() {
+    setTimeout(function() {
+        console.log( 'Кофе готов!' );
+    }, this._getTimeToBoil());
+};
+
+CoffeeMachine.prototype.setWaterAmount = function(amount) {
+    this._waterAmount = amount;
+};
+
+var coffeeMachine = new CoffeeMachine(10000);
+coffeeMachine.setWaterAmount(50);
+coffeeMachine.run();
